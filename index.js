2e595a44a3e998777e216ea159b04a1e83536049
Lawrence-drew <103955384+Lawrence-drew@users.noreply.github.com>
2024-05-02 23:07:41 +0100

    Added files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const dotenv = require("dotenv").config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()  =>{
+    console.log(`Server started running on ${PORT}`)
+})
+
+app.get("/string", (request, response)=>{
+    response.json("Sending back a simple string as the response")
+})
+
+app.get("/object", (request, response)=>{
+    response.json({
+        "name": "Lawrence Onyekachukwu Gift"
+         })
+})
+
+app.get("/array", (request, response)=>{
+    response.json(["Access Bank", "Union Bank", "Wema Bank", "Zenith Bank"])
+})
