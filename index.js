8f10f8b57926169c70598009422d494f4cff2c17
Lawrence-drew <103955384+Lawrence-drew@users.noreply.github.com>
2024-05-02 23:38:29 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv").config()
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = 8100
 
 app.listen(PORT, ()  =>{
     console.log(`Server started running on ${PORT}`)
